14fb0a765c722fffed2ec8775cc2ce29808a4d01
MERCE-DEATH <masters1004@gmail.com>
2024-09-07 11:53:31 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<!-- JS -->
+<script setup>
+import { ref } from 'vue';
+/**
+todo item {
+  text: string
+  done : boolean
+}
+ */
+const inputValue = ref('');
+const todoList = ref([{ text : "vue", done : false }, { text : "react", done : false }, { text : "vercel", done : false }]);
+
+console.log(inputValue.value);
+const printInputValue = () => {
+  console.log(inputValue.value);
+};
+
+const handleClickButton = () => {
+  todoList.value.push(inputValue.value);
+};
+
+const handleChange = (event) => {
+ const nextValue = event.target.value;
+ inputValue.value = nextValue;
+}
+
+const handleClickDeleteButton = (index) => {
+  todoList.value.slice(index, 1);
+}
+</script>
+
+<!-- html -->
+<template>
+  <h4>TODO LIST</h4>
+  <p>{{ todoList }}</p>
+
+  <!-- <input v-model="inputValue"/> -->
+  <input v-model="inputValue" @change="handleChange"/>
+  <button @click = "handleClickButton">확인</button>
+
+  <div class="todo-item" v-for="(item, idx) in todoList">
+    <input type="checkbox" v-model="item.done"/>
+    <span :class="{ 'done-item' : tiem.done }">
+      {{ item.text }}
+    </span>
+
+    <div>
+      <button>수정</button>
+      <button @click = "handleClickDeleteButton(idx)">삭제</button>
+    </div>
+  </div>
+</template>
+
+<!-- css -->
+<style scoped>
+.todo-item {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid gray;
+}
+
+.done-item {
+  text-decoration: line-through;
+}
+</style>
